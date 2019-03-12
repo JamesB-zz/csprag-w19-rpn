c2ac0425aafa1a183b63f4774cf03de5ed5c263e
jaimb <jaimb@umich.edu>
2019-03-12 09:54:25 -0400

    Added color to RPN and a modulus test

--- rpn.py
+++ rpn.py
@@ -2,6 +2,7 @@
 
 import operator
 
+from termcolor import colored
 
 operators = {
     '+': operator.add,
@@ -17,21 +18,27 @@ def calculate(myarg):
         try:
             token = int(token)
             stack.append(token)
+            print
         except ValueError:
             function = operators[token]
             arg2 = stack.pop()
+            col2 = 'magenta' if arg2<0 else 'white'
             arg1 = stack.pop()
+            col1 = 'magenta' if arg1<0 else 'white'
             result = function(arg1, arg2)
+            colres = 'red' if result<0 else 'white'
+            text = colored(arg1,col1) + colored(token,'yellow') + colored(arg2, col2) + colored('=', 'green') + colored(result,colres)
+            print(text)
             stack.append(result)
-        print(stack)
     if len(stack) != 1:
         raise TypeError("Too many parameters")
     return stack.pop()
 
 def main():
+    print(colored('Welcome to the PRN calculator!', 'blue'))
     while True:
         result = calculate(input("rpn calc> "))
-        print("Result: ", result)
+        print("Result: ", colored(result, 'cyan'))
 
 if __name__ == '__main__':
     main()
--- test_rpn.py
+++ test_rpn.py
@@ -18,3 +18,6 @@ class TestBasics(unittest.TestCase):
     def test_exponential(self):
         result = rpn.calculate("2 2 ^")
         self.assertEqual(4,result)
+    def test_modulus(self):
+        result = rpn.calculate("3 4 %")
+        self.assertEqual(3,result)
